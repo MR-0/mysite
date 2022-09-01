7dd77fea8ff693f2e6623879d03e4c323977bb71
MR-0 <wario.mail@gmail.com>
2022-09-01 16:34:56 -0400

    Remove artifact registry block

--- infra/main.tf
+++ infra/main.tf
@@ -6,10 +6,6 @@ variable "region" {
   type     = string
   nullable = false
 }
-variable "docker_artifact_path" {
-  type     = string
-  nullable = false
-}
 
 terraform {
   required_providers {
@@ -28,10 +24,4 @@ provider "google" {
   project = var.project
   region  = var.region
   zone    = "us-central1-c"
-}
-
-resource "google_artifact_registry_repository" "images" {
-  location      = var.region
-  repository_id = var.docker_artifact_path
-  format        = "DOCKER"
 }
