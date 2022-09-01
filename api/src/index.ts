efe64ed8754543a55d066d67d9e76950eb29dc7d
MR-0 <wario.mail@gmail.com>
2022-08-31 22:35:52 -0400

    Use a estatic port

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,7 @@
 import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = 8080;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
