9dc74c451efc722e557a8db4fd094859578f403d
MR-0 <wario.mail@gmail.com>
2022-09-01 19:14:28 -0400

    Allow no autenticate

--- infra/main.tf
+++ infra/main.tf
@@ -48,3 +48,17 @@ resource "google_cloud_run_service" "api" {
     latest_revision = true
   }
 }
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role    = "roles/run.invoker"
+    members = ["allUsers"]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.api.location
+  project     = google_cloud_run_service.api.project
+  service     = google_cloud_run_service.api.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
