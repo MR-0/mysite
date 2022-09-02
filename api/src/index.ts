8b8470714544124635b840dde7ffc40f4c909903
MR-0 <wario.mail@gmail.com>
2022-09-02 18:45:10 -0400

    Add json middleware and some endpoints
    get brands, get brand, get brand by id and post brands endpoints

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,38 @@
 import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
+const brands = [
+  { id: '1', name: 'Brand 1', image: 'bucket/images/brand_id/image.jpg'}
+]
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.send('Hola');
+});
+
+app.get('/brands', (_req, res) => {
+  res.send(brands);
+});
+
+app.get('/brands/:id', (req, res) => {
+  const brand = brands.find(({ id }) => id === req.params.id);
+  if (!brand) res.sendStatus(404);
+  else res.send(brand);
+});
+
+app.post('/brands', (req, res) => {
+  const brand = {
+    id: brands.length + 1,
+    ...req.body
+  };
+  brands.push(brand)
+  res.send(brand);
 });
 
 app.listen(port, () => {
