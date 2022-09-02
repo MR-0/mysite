2612b4258feac80e6b2b104a876bbca1b3ab57f6
MR-0 <wario.mail@gmail.com>
2022-09-02 19:26:13 -0400

    return only the element deleted

--- api/src/index.ts
+++ api/src/index.ts
@@ -48,7 +48,7 @@ app.put('/brands/:id', (req, res) => {
 app.delete('/brands/:id', (req, res) => {
   const index = brands.map(({ id }) => id).indexOf(req.params.id);
   if (index < 0) return res.sendStatus(404);
-  res.send(brands.splice(index, 1));
+  res.send(brands.splice(index, 1).pop());
 });
 
 app.listen(port, () => {
