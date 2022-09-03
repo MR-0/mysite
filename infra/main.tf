2736323a5f117b41af08b73890076a328e5f9ed3
MR-0 <wario.mail@gmail.com>
2022-09-03 19:04:20 -0400

    Fix error in max scale cloud run

--- infra/main.tf
+++ infra/main.tf
@@ -44,7 +44,7 @@ resource "google_cloud_run_service" "api" {
     metadata {
       annotations = {
         "autoscaling.knative.dev/minScale" = "1"
-        "autoscaling.knative.dev/minScale" = "10"
+        "autoscaling.knative.dev/maxScale" = "10"
       }
     }
   }
